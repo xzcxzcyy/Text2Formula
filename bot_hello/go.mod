27586e56e6cc6cbad52d57570435ca765d76fed5
Banson Xu <xzcxzcyy@126.com>
2021-07-17 22:07:52 +0800

    initial

---
+++ bot_hello/go.mod
@@ -0,0 +1,5 @@
+module bot_hello
+
+go 1.16
+
+require gopkg.in/tucnak/telebot.v2 v2.3.5
---
+++ bot_hello/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	tb "gopkg.in/tucnak/telebot.v2"
+	"log"
+	"strconv"
+	"time"
+)
+
+func main() {
+	b, err := tb.NewBot(tb.Settings{
+		// You can also set custom API URL.
+		// If field is empty it equals to "https://api.telegram.org".
+		URL: "https://api.telegram.org",
+
+		Token:  "1706279559:AAH3xrI-U2qRg3SdwJkAcQ09OoAF2FMpVwU",
+		Poller: &tb.LongPoller{Timeout: 10 * time.Second},
+	})
+
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	b.Handle("/hello", func(m *tb.Message) {
+		b.Send(m.Sender, m.Payload)
+	})
+
+	b.Handle(tb.OnQuery, func (q *tb.Query) {
+		log.Println(q.Text)
+		urls := []string{
+			"https://t0922496-nus-13072021.s3.amazonaws.com/f11.jpg",
+			"https://t0922496-nus-13072021.s3.amazonaws.com/f12.jpg",
+		}
+	
+		results := make(tb.Results, len(urls)) // []tb.Result
+		for i, url := range urls {
+			result := &tb.PhotoResult{
+				ResultBase:  tb.ResultBase{},
+				URL:         url,
+				Width:       0,
+				Height:      0,
+				Title:       "",
+				Description: "",
+				Caption:     q.Text,
+				ParseMode:   "",
+				ThumbURL:    url,
+				Cache:       "",
+			}
+
+			results[i] = result
+			// needed to set a unique string ID for each result
+			results[i].SetResultID(strconv.Itoa(i))
+		}
+	
+		err := b.Answer(q, &tb.QueryResponse{
+			Results:   results,
+			CacheTime: 10, // a minute
+		})
+	
+		if err != nil {
+			log.Println(err)
+		}
+	})
+
+	b.Start()
+}
+
