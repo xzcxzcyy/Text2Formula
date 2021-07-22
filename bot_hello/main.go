5ada0bf76dd21237b8357df45e9118d225c18e1e
Banson Xu <xzcxzcyy@126.com>
2021-07-22 17:32:42 +0800

    refactor: move render together

--- bot_hello/main.go
+++ bot_hello/main.go
@@ -3,10 +3,8 @@ package main
 import (
     "fmt"
     tb "gopkg.in/tucnak/telebot.v2"
-    "image"
     "log"
     "os"
-    "os/exec"
     "strconv"
     "time"
 )
@@ -121,37 +119,4 @@ func main() {
     })
 
     myBot.Start()
-}
-
-func renderTex(queryID string, formula string) (filePath string, sizeInfo image.Point, retErr error) {
-    wd, err := os.Getwd()
-    if err != nil {
-        log.Printf("err occurs when os.Getwd(): %v.", err)
-        return "", image.Point{}, err
-    }
-    curSvgFilePath := wd + "/svg/" + queryID + ".svg"
-    curPngFilePath := wd + "/png/" + queryID + ".png"
-    curJpgFilePath := wd + "/jpg/" + queryID + ".jpg"
-    perfTex2Svg := exec.Command(wd+"/mathjax/tex2svg", formula, curSvgFilePath)
-    //log.Println(perfTex2Svg.Args)
-    perfTex2Svg.Dir = wd + "/mathjax"
-    err = perfTex2Svg.Run()
-    if err != nil {
-        log.Printf("during perfTex2Svg: %v", err)
-        return "", image.Point{}, err
-    }
-    //log.Println(perfTex2Svg.Args)
-    perfSvg2Png := exec.Command("cairosvg", curSvgFilePath, "-o", curPngFilePath, "--output-height", "360")
-    //log.Println(perfSvg2Png.Args)
-    err = perfSvg2Png.Run()
-    if err != nil {
-        log.Printf("during perfSvg2Png: %v", err)
-        return "", image.Point{}, err
-    }
-    imgSize, err := png2Jpg(curPngFilePath, curJpgFilePath)
-    if err != nil {
-        log.Printf("during png2Jpg: %v", err)
-        return "", image.Point{}, err
-    }
-    return curJpgFilePath, imgSize, nil
 }
--- bot_hello/render.go
+++ bot_hello/render.go
@@ -8,6 +8,7 @@ import (
     "image/png"
     "log"
     "os"
+    "os/exec"
 )
 
 func png2Jpg(pngFilePath string, jpgFilePath string) (sizeInfo image.Point, retErr error) {
@@ -63,4 +64,37 @@ func png2Jpg(pngFilePath string, jpgFilePath string) (sizeInfo image.Point, retE
 
     //log.Println("Converted PNG file to JPEG file")
     return newImg.Bounds().Size(), nil
+}
+
+func renderTex(queryID string, formula string) (filePath string, sizeInfo image.Point, retErr error) {
+    wd, err := os.Getwd()
+    if err != nil {
+        log.Printf("err occurs when os.Getwd(): %v.", err)
+        return "", image.Point{}, err
+    }
+    curSvgFilePath := wd + "/svg/" + queryID + ".svg"
+    curPngFilePath := wd + "/png/" + queryID + ".png"
+    curJpgFilePath := wd + "/jpg/" + queryID + ".jpg"
+    perfTex2Svg := exec.Command(wd+"/mathjax/tex2svg", formula, curSvgFilePath)
+    //log.Println(perfTex2Svg.Args)
+    perfTex2Svg.Dir = wd + "/mathjax"
+    err = perfTex2Svg.Run()
+    if err != nil {
+        log.Printf("during perfTex2Svg: %v", err)
+        return "", image.Point{}, err
+    }
+    //log.Println(perfTex2Svg.Args)
+    perfSvg2Png := exec.Command("cairosvg", curSvgFilePath, "-o", curPngFilePath, "--output-height", "360")
+    //log.Println(perfSvg2Png.Args)
+    err = perfSvg2Png.Run()
+    if err != nil {
+        log.Printf("during perfSvg2Png: %v", err)
+        return "", image.Point{}, err
+    }
+    imgSize, err := png2Jpg(curPngFilePath, curJpgFilePath)
+    if err != nil {
+        log.Printf("during png2Jpg: %v", err)
+        return "", image.Point{}, err
+    }
+    return curJpgFilePath, imgSize, nil
 }
